8c4463cbac2a79f19b5fecf77c9b2660d6ffc125
Supraja Tangella <supraja.tangella@gmail.com>
2024-10-30 15:12:21 +0000

    Add project files.

---
+++ PrimaryConstrucorDemo/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimaryConstrucorDemo
+{
+    public class Product(string name, decimal price)
+    {
+        public string Name { get; } = name;
+        public decimal Price { get; } = price;
+        public void PrintDetails() => Console.WriteLine($"{Name}: ${Price}");
+    }
+}
---
+++ PrimaryConstrucorDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using PrimaryConstrucorDemo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimaryConstructorDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var product = new Product("Laptop", 1200);
+            product.PrintDetails();  // Output: Laptop: $1200
+            Console.ReadLine();
+        }
+    }
+}
